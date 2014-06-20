5c47b24c4d5a10d3478b89d2a60bafa8b3328525
Michal Špaček <tupinek@gmail.com>
2014-06-20 10:56:20 +0200

    Add example to doc.

--- Config.pm
+++ Config.pm
@@ -416,9 +416,81 @@ Constructor.
  use warnings;
 
  # Modules.
+ use Data::Printer;
  use Image::Keyboard::Config;
 
- # TODO
+ # Object.
+ my $conf = Image::Keyboard::Config->new(
+        'b_image' => 'data/ex1/keyboard_button_small.png',
+        'background_file' => 'data/ex1/keyboard_background.png',
+        'f_file' => 'data/ex1/DejaVuSans.ttf',
+ );
+
+ # Draw two buttons.
+ $conf->draw->move(100, 100)->draw;
+
+ # Get configuration.
+ my $conf_hr = $conf->get;
+
+ # Dump configuration.
+ p $conf_hr;
+
+ # Output:
+ # \ {
+ #     background   "data/ex1/keyboard_background.png",
+ #     button       {
+ #         1   {
+ #             font    {
+ #                 color   "black",
+ #                 file    "data/ex1/DejaVuSans.ttf",
+ #                 size    20
+ #             },
+ #             image   "data/ex1/keyboard_button_small.png",
+ #             pos     {
+ #                 left   0,
+ #                 top    0
+ #             },
+ #             text    {
+ #                 align     {
+ #                     horz   "center",
+ #                     vert   "middle"
+ #                 },
+ #                 padding   {
+ #                     bottom   0,
+ #                     left     0,
+ #                     right    0,
+ #                     top      0
+ #                 },
+ #                 string    "Foo"
+ #             }
+ #         },
+ #         2   {
+ #             font    {
+ #                 color   "black",
+ #                 file    "data/ex1/DejaVuSans.ttf",
+ #                 size    20
+ #             },
+ #             image   "data/ex1/keyboard_button_small.png",
+ #             pos     {
+ #                 left   100,
+ #                 top    100
+ #             },
+ #             text    {
+ #                 align     {
+ #                     horz   "center",
+ #                     vert   "middle"
+ #                 },
+ #                 padding   {
+ #                     bottom   0,
+ #                     left     0,
+ #                     right    0,
+ #                     top      0
+ #                 },
+ #                 string    "Foo"
+ #             }
+ #         }
+ #     }
+ # }
 
 =head1 DEPENDENCIES
 
